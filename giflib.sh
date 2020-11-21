47ed721877c931e646d3f75e64c7b056dc8c6f0d
leleliu008 <792793182@qq.com>
2020-11-21 16:07:09 +0800

    optimize

--- giflib.sh
+++ giflib.sh
@@ -4,8 +4,8 @@ url="https://downloads.sourceforge.net/project/giflib/giflib-5.2.1.tar.gz"
 sha256="31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd"
 
 prepare() {
-    sed_in_place 's#$(MAKE) -C doc#@mkdir -p doc \&\& ([ -f doc/giflib.1 ] || touch doc/giflib.1)#' Makefile &&
-    sed_in_place 's#soname#install_name#g' Makefile
+    sed_in_place 's#$(MAKE) -C doc#@mkdir -p doc \&\& ([ -f doc/giflib.1 ] || touch doc/giflib.1)#' Makefile
+    #sed_in_place 's#soname#install_name#g' Makefile
 }
 
 build() {
