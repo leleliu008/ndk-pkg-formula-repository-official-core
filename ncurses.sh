9517d874c1b0a40daaac5a32d6e2909e736cf738
leleliu008 <792793182@qq.com>
2021-01-06 16:58:03 +0800

    optimize

--- ncurses.sh
+++ ncurses.sh
@@ -2,19 +2,43 @@ summary "Text-based UI library"
 webpage "https://www.gnu.org/software/ncurses"
 src_url "https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz"
 src_sum "30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
+require "sed grep make"
+
+need_native_build() {
+    # https://github.com/termux/termux-packages/issues/4487
+    if command -v tic > /dev/null ; then
+        [ '6.2.20200212' != "$(list 6.2.20200212 $(tic -V | cut -d' ' -f2) | sort -V | head -n 1)" ]
+    fi
+}
+
+prepare() {
+    if need_native_build ; then
+        echo "Native building..." &&
+        cd $BUILD_DIR &&
+        $SOURCE_DIR/configure --prefix=$PWD/output &&
+        $MAKE clean &&
+        $MAKE &&
+        $MAKE install &&
+        export PATH=$PWD/output/bin:$PATH
+    fi
+}
 
 build() {
     configure \
-        --without-pkg-config \
-        --without-tests \
+        --with-pkg-config-libdir="$ABI_PKGCONF_DIR" \
+        --with-shared \
         --without-ada \
+        --without-tests \
         --without-debug \
         --without-valgrind \
-        --with-shared \
+        --enable-const \
+        --enable-widec \
+        --enable-termcap \
         --enable-warnings \
         --enable-pc-files \
-        --with-pkg-config-libdir="$ABI_PKGCONF_DIR" \
-        --enable-stripping \
+        --enable-ext-mouse \
+        --enable-ext-colors \
+        --disable-stripping \
         --disable-assertions \
         --disable-gnat-projects \
         --disable-echo &&
