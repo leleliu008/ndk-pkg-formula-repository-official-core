0c90bcf08755ae3f78478150bef90c749a8d1ac3
leleliu008 <792793182@qq.com>
2021-01-09 22:24:47 +0800

    optimize

--- unrar.sh
+++ unrar.sh
@@ -1,10 +1,11 @@
 summary "Extract, view, and test RAR archives"
-webpage "https://www.rarlab.com/"
+webpage "https://www.rarlab.com"
 src_url "https://www.rarlab.com/rar/unrarsrc-6.0.2.tar.gz"
 src_sum "81bf188333f89c976780a477af27f651f54aa7da9312303d8d1a804696d3edd3"
 require "make"
 
 # int lutimes(const char* __path, const struct timeval __times[2]) __INTRODUCED_IN(26);
+sdk_api 26
 
 prepare() {
     sed_in_place 's|getpass("")|""|g' consio.cpp &&
