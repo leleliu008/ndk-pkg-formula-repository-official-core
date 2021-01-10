c9640513b1ecbb70b2d13e67991271300f7959c7
leleliu008 <792793182@qq.com>
2021-01-10 19:18:27 +0800

    optimize

--- suricata.sh
+++ suricata.sh
@@ -5,7 +5,7 @@ src_sum "34413ecdad2ff2452526dbcd22f1279afd0935151916c0ff9cface4b0b5665db"
 license "GPL-2.0"
 require "rustup cargo make"
 depends "jansson file libnet libpcap libyaml lz4 pcre libiconv libglob"
-ldfalgs "-lglob"
+ldflags "-lglob"
 
 # void endgrent(void) __INTRODUCED_IN(26);
 sdk_api 26
@@ -18,9 +18,12 @@ build() {
     RUST_TARGET=$(echo "$BUILD_FOR_TARGET" | sed 's/armv7a/armv7/')
     rustup target add "$RUST_TARGET" &&
     sed_in_place "/host_triplet = /c host_triplet = $RUST_TARGET" "$SOURCE_DIR/rust/Makefile.in" &&
-    sed_in_place "/RUST_SURICATA_LIB =/RUST_SURICATA_LIB = ../rust/target/$RUST_TARGET/release/libsuricata.a" "$SOURCE_DIR/src/Makefile.in" &&
+    sed_in_place '/RUST_LDADD="${RUST_SURICATA_LIB}/i RUST_SURICATA_LIB=$(echo "$RUST_SURICATA_LIB" | sed "s/armv7a/armv7/")' "$SOURCE_DIR/configure" &&
+    sed_in_place '/RUST_LDADD="${RUST_SURICATA_LIB}/a RUST_LDADD=$(echo "$RUST_LDADD" | sed "s/-lrt//")' "$SOURCE_DIR/configure" &&
     configure \
         --enable-pie \
+        ac_cv_func_malloc_0_nonnull=yes \
+        ac_cv_func_realloc_0_nonnull=yes \
         ac_cv_lib_pthread_pthread_create=yes \
         ac_cv_lib_jansson_json_dump_callback=yes \
         ac_cv_lib_pcap_pcap_open_live=yes
