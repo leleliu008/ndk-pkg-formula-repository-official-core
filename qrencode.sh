9708cff545decdbacbd3bdfdf36c6558402a024e
leleliu008 <792793182@qq.com>
2021-02-04 19:30:48 +0800

    optimize

--- qrencode.sh
+++ qrencode.sh
@@ -2,6 +2,7 @@ summary "QR Code generation"
 webpage "https://fukuchi.org/works/qrencode"
 src_url "https://fukuchi.org/works/qrencode/qrencode-4.0.2.tar.gz"
 src_sum "dbabe79c07614625d1f74d8c0ae2ee5358c4e27eab8fd8fe31f9365f821a3b1d"
+require "cmake"
 depends "libpng"
 
 build() {
@@ -11,9 +12,5 @@ build() {
     -DWITHOUT_PNG=OFF \
     -DPNG_PNG_INCLUDE_DIR="$libpng_INCLUDE_DIR" \
     -DPNG_LIBRARY_RELEASE="$libpng_LIBRARY_DIR/libpng.so" \
-    -DZLIB_LIBRARY_RELEASE="$SYSTEM_LIBRARY_DIR/libz.so"
-}
-
-build2() {
-    configure
+    -DZLIB_LIBRARY_RELEASE="$zlib_LIBRARY_DIR/libz.so"
 }
--- unzip.sh
+++ unzip.sh
@@ -5,7 +5,7 @@ src_sum "036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
 version "6.0-25"
 require "patch tar"
 depends "zip bzip2"
-patches "https://mirrors.tuna.tsinghua.edu.cn/debian/pool/main/u/unzip/unzip_$(version).debian.tar.xz" \
+patches "https://deb.debian.org/debian/pool/main/u/unzip/unzip_$(version).debian.tar.xz" \
         "0783e4d11d755cb43904e3f59a60dbb92ee9c6b08ac54d86bc61f9848216f37b"
 # https://packages.debian.org/buster/unzip
 
