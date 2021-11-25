0603edffffed942f88aa7224f5485b5ce42ac9a7
leleliu008 <leleliu008@gmail.com>
2021-11-25 15:56:37 +0800

    optimized

--- formula/zola.sh
+++ formula/zola.sh
@@ -5,3 +5,8 @@ package set src.url "https://github.com/getzola/zola/archive/v0.14.1.tar.gz"
 package set src.sum "28e50071009a1430c5f8df94e2585d095f85f906f04101fe35ee9ed53c353cc4"
 package set license "MIT"
 package set bsystem "cargo"
+
+prepare() {
+    export CARGO_HTTP_MULTIPLEXING=false
+    #export RUSTFLAGS="-lc++_static"
+}
