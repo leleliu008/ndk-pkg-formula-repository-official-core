2baa33f5c64c56eb83efc66d9b507479c786c024
leleliu008 <leleliu008@gmail.com>
2022-04-03 19:38:06 +0800

    optimized

--- formula/libksba.sh
+++ formula/libksba.sh
@@ -3,8 +3,8 @@ package set webpage "https://www.gnupg.org/related_software/libksba"
 package set src.url "https://gnupg.org/ftp/gcrypt/libksba/libksba-1.6.0.tar.bz2"
 package set src.sum "dad683e6f2d915d880aa4bed5cea9a115690b8935b78a1bbe01669189307a48b"
 package set license "GPL-2.0"
-package set bsystem "configure"
 package set dep.pkg "libgpg-error"
+package set bsystem "configure"
 
 build() {
     configure \
