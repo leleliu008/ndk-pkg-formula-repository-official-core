f7bda10ba6b30d25c0b3cafa308589249bb5d93f
leleliu008 <leleliu008@gmail.com>
2022-05-08 14:33:33 +0800

    optimized

--- formula/pkg-config.sh
+++ formula/pkg-config.sh
@@ -3,8 +3,8 @@ pkg_set webpage "https://freedesktop.org/wiki/Software/pkg-config"
 pkg_set src.url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz"
 pkg_set src.sha "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
 pkg_set license "GPL-2.0-or-later"
-pkg_set bsystem "configure"
 pkg_set dep.pkg "libiconv"
+pkg_set bsystem "configure"
 
 prepare() {
     CACHE_FILE="$PACKAGE_BSCRIPT_DIR/glib/configure.cache"
