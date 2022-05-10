b2370967e74f961c1622a958a3e47b302bc1aa1c
leleliu008 <leleliu008@gmail.com>
2022-05-10 16:43:05 +0800

    optimized

--- formula/jfrog-cli.sh
+++ formula/jfrog-cli.sh
@@ -1,8 +1,8 @@
 pkg_set summary "Command-line interface for Jfrog Artifactory and Bintray"
 pkg_set webpage "https://www.jfrog.com/confluence/display/CLI/JFrog+CLI"
 pkg_set git.url "https://github.com/jfrog/jfrog-cli.git"
-pkg_set src.url "https://github.com/jfrog/jfrog-cli/archive/v2.16.2.tar.gz"
-pkg_set src.sha "9c4da58c688d5d680602c4ac12505ac7a4bbaaffe1d7dc3a788ea08a087aa29f"
+pkg_set src.url "https://github.com/jfrog/jfrog-cli/archive/v2.16.3.tar.gz"
+pkg_set src.sha "3ab0927d0cbabfc6eac88033edbb1ad7c328cd5b2110ac1b0b0a986fbcc9b593"
 pkg_set license "Apache-2.0"
 pkg_set bsystem "go"
 
